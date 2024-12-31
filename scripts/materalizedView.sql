b1e9b676b3a6d15538e9cfedb5b09df9ed52bc5d
Rafael Vieira <mouravieira44@gmail.com>
2024-12-31 11:36:17 -0300

    add 2 materalized view

--- scripts/materalizedView.sql
+++ scripts/materalizedView.sql
@@ -7,3 +7,20 @@ FROM PERFIL p
 JOIN AMIGOS a ON p.cp_id_perfil = a.ce_id_perfil
 JOIN DESEJO d ON a.ce_id_amigo = d.ce_id_perfil;
 
+-- 2.
+-- Resumo das Bibliotecas com Informações de Categoria e Usuário
+CREATE MATERIALIZED VIEW mv_resumo_bibliotecas AS
+SELECT
+    b.cp_id_biblioteca,
+    b.nome AS nome_biblioteca,
+    b.tempo_uso,
+    c.nome AS nome_categoria,
+    p.nome AS nome_usuario,
+    p.apelido AS apelido_usuario,
+    p.email AS email_usuario
+FROM
+    BIBLIOTECA b
+JOIN
+    CATEGORIA c ON b.ce_id_categoria = c.cp_id_categoria
+JOIN
+    PERFIL p ON b.ce_id_perfil = p.cp_id_perfil;
